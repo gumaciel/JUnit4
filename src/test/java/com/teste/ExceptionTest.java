a84125a366fa7394e081961869f451e27f015c76
Gustavo Maciel <gustamaciel2012@hotmail.com>
2021-03-06 15:14:19 -0300

    Arquivos adicionados

---
+++ src/test/java/com/teste/ExceptionTest.java
@@ -0,0 +1,44 @@
+package com.teste;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.ArrayList;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class ExceptionTest {
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void empty() {
+		List<String> lista = new ArrayList<String>();
+		lista.get(0);
+	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void shouldTestExceptionMessage() throws IndexOutOfBoundsException {
+		List<Object> list = new ArrayList<Object>();
+		
+		thrown.expect(IndexOutOfBoundsException.class);
+		thrown.expectMessage("Index 0 out of bounds for length 0");
+		list.get(0);
+	}
+	
+	@Test
+	public void testExceptionMessage() {
+		try {
+			new ArrayList<Object>().get(0);
+			fail("Esperado que o IndexOutOfBoundsException seja lançado");
+		}catch (IndexOutOfBoundsException e) {
+			assertThat(e.getMessage(), is("Index 0 out of bounds for length 0"));
+		}
+	}
+}
